c7571c8700080f59c69eea0a07e18c929665069e
MahanthiSriAsish1 <110473671+MahanthiSriAsish1@users.noreply.github.com>
2023-09-25 00:14:46 +0530

    made changes to carousel code

--- src/App.js
+++ src/App.js
@@ -5,19 +5,11 @@ import Carousel from './Components/Body';
 import Card from './Components/Card';
 import Navbar from './Components/Header';
 import './css/Carousel.css';
-
-const images = [
-  './Dairy-Farming-compressed.jpg',
-  './jacqueline-o-gara-mRhDiFUzIoE-unsplash.jpg',
-  './vadim-kaipov-8ZELrodSvTc-unsplash.jpg',
-];
-
 function App() {
   return (
     <div className="App">
        <Navbar /> 
-      <h1>Image Carousel</h1>
-      <Carousel images={images} />
+      <Carousel />
       <div className="card-container">
         <Card
           title="Card 1"
